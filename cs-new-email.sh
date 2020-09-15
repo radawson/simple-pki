d32e2c47426cdc2c8c872998eb493428ee303227
Richard A. Dawson <dawsonra@intranet.partridgexing.org>
2020-09-14 20:02:49 -0400

    Added TLS signing script

--- cs-new-email.sh
+++ cs-new-email.sh
@@ -5,7 +5,7 @@
 if [[ $1 =~ ^[0-9a-zA-Z_-]+$ ]];then
 
 	# Create the CS request
-	# The openssl req command takes its configuration from the [req] section of the root-ca.conf file.
+	# The openssl req command takes its configuration from the [req] section of the email.conf file.
 	# You will be asked for a passphrase to protect the private key.
 	echo -e "Creating the Email request"
 	openssl req -new \
@@ -15,7 +15,7 @@ if [[ $1 =~ ^[0-9a-zA-Z_-]+$ ]];then
 	echo -e "Your private key is located at etc/certs/"$1".key"
 	
 	# Create the Email certificate
-	# The openssl ca command takes its configuration from the [ca] section of the root-ca.conf file.
+	# The openssl ca command takes its configuration from the [ca] section of the email.conf file.
 	echo -e "Creating the Email certificate"
 	openssl ca \
 		-config etc/signing-ca.conf \
